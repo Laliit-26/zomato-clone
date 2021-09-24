7fbce844e6b01cf2d0e50e79e16eb01f3ccedf25
lalit <tiwarilalit2601@gmail.com>
2021-09-24 22:20:36 +0530

    zomato clone with react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import header from './component/header';
+
+function App() {
+  return (
+    <div className="App">
+<header/>
+   
+    </div>
+  );
+}
+
+export default App;
